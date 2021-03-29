90da7d497b1e766d1874ef87390f27bcb640bcfe
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-28 19:53:16 -0700

    Added a preload file

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const puppeteer = require("puppeteer");
+const preloadFile = fs.readFileSync('./preload.js', 'utf8');
 
 (async () => {
-  const browser = await puppeteer.launch({
-      headless: false
-  });
+
+  const args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-infobars',
+    '--window-position=0,0',
+    '--ignore-certifcate-errors',
+    '--ignore-certifcate-errors-spki-list',
+    '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+];
+
+  const options = {
+      args,
+      headless: false,
+      ignoreHTTPSErrors: true,
+      userDataDir: './tmp'
+  };
+  const browser = await puppeteer.launch(options);
 
   const page = await browser.newPage();
 
-  await page.goto("https://www.walgreens.com/");
-  // await page.goto("https://www.paulreinventswheels.com");
+  await page.evaluateOnNewDocument(preloadFile);
+ 
+  await page.goto("https://www.walgreens.com/findcare/vaccination/covid-19/location-screening");
+  
   await page.setViewport({ width: 1200, height: 800 });
 
-  // await page.waitForSelector(".topbar > input");
-  // await page.click(".topbar > input");
+  await page.click(".pac-target-input");
+  await page.type(".pac-target-input", 'hello', { delay: 20 });
 
-  await page.waitForSelector(".card__content");
-  await page.click(".card__content");
-
-  await page.waitForSelector(".getstarted-btn");
-  await page.click(".get-started-btn");
 
   // await page.type(".topbar > input", "for");
 

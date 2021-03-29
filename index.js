ef894fadf687a934dff1a72a338de7ccc4e6d069
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-28 18:58:17 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const url = 'https://www.reddit.com';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    console.log(html);
+  })
+  .catch(function(err) {
+    //handle error
+  });

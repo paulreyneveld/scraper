4a0da96009f4766001cdca13432c4e1ef6edf603
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-28 19:01:04 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const puppeteer = require('puppeteer');
-const url = 'https://www.reddit.com';
+const puppeteer = require("puppeteer");
 
-puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  })
-  .then(function(html) {
-    console.log(html);
-  })
-  .catch(function(err) {
-    //handle error
-  });
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://www.walgreens.com/findcare/vaccination/covid-19/location-screening");
+
+  await page.setViewport({ width: 1200, height: 800 });
+
+  // await page.waitForSelector(".topbar > input");
+  // await page.click(".topbar > input");
+
+  // await page.type(".topbar > input", "for");
+
+  // await page.waitForSelector("#button-search");
+  // await page.click("#button-search");
+
+  await browser.close();
+})();
